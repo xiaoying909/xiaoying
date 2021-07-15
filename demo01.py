18436d47b61bb6ffad5333c56981248b97812f60
xiaoying <1595746691@qq.com>
2021-07-15 17:21:14 +0800

    第一次提交Giuhub提交代码

---
+++ demo01.py
@@ -0,0 +1,22 @@
+""""
+print("hello world！")   #字符串
+print(2333)     #整数
+print(2.3333)      #小数
+print(True)              #布尔值 Ture False
+print(())             #元组
+print({})             #字典
+print([])           #数组
+print("憨憨"+"嘻嘻")#字符串的拼接
+print("憨憨"*100)
+print(1+2)
+print(2<3)
+name = "张三"                #将张三这个值赋值给了变量a（变量是小写字母）
+print(name)
+"""""
+a = str(input("请输入："))
+b = str(input("请输入："))
+print("inpurt获取的值",len(a+b))
+#数据格式的转换
+# type(数据)   查询数据格式
+
+
